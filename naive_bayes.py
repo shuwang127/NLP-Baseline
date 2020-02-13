8a111a9ff1cf5acbfb4f082670619d962d8c7e0d
Shu W <shuwang127@yahoo.com>
2020-02-13 18:47:45 -0500

    Add files via upload

---
+++ naive_bayes.py
@@ -0,0 +1,16 @@
+# Author: Julia Jeng, Shu Wang, Arman Anwar
+# Description: AIT 726 Homework 1
+# Command to run the file:
+#
+
+import nltk
+
+# The main function.
+def main():
+    print("-- AIT726 Homework 1 from Julia Jeng, Shu Wang, and Arman Anwar --");
+
+
+# The program entrance.
+if __name__ == "__main__":
+    main()
+
